9640202ca945beea02912d70d1b4d2508c0ab2ae
dplayerf <118938881+dplayerf@users.noreply.github.com>
2024-09-06 19:30:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Lenilson",
+        descricao: "Nascido em São Sebastião de Lagoa de Roça - PB, Influxking é um dos membros mais ativos do servidor Gangue do ventilador, sendo o ADM lider e sem saber dar 200 no fortnite ele é um dos membros mais importantes da gangue do ventilador e fundador da mesma.",
+        link: "https://www.instagram.com/lenil272",
+        tags: "gay amoeba player banana"
+    },
+    {
+        titulo: "João",
+        descricao: "Nascido no Paraná, João é um dos membros mais ativos no servidor, conhecido por adorar animes de garotinhas que tocam e cantam rock ele também é um dos melhores players de AMQ e Fortnite atualmente. Esse cara também é um dos ADMs do servidor e também a webnamorada do influxking.",
+        link: "https://www.instagram.com/jaoxn99",
+        tags: "insuportavel chato feio"
+    },
+    {
+        titulo: "Dplayerf",
+        descricao: "Nascido em São Sebastião de Lagoa de Roça - PB, Dp como também o chamam é um membro ativo e não ativo do servidor Gangue do ventilador, conhecido por sua fama de sonegar impostos, esse membro é considerado um dos fundadores do servidor e um pilar entre todos, por ter seu humor duvidoso e esquisito ele é um cara muito gente boa, lindo e atraente e possui a skill namoradada",
+        link: "https://www.instagram.com/daniel2mota",
+        tags: "lindo maravilhoso gostoso gente boa"
+    }
+];
